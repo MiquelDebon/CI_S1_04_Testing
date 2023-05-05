231e1dcf5d08dae6d52564b912e65b8230cc0df4
Miquel Debón Villagrasa <mdebonbcn@gmail.com>
2023-05-05 14:01:03 +0200

    N3 done

--- src/test/java/S1_04_N2_Hamcret_Matcher/LongitudMathcer.java
+++ src/test/java/S1_04_N2_Hamcret_Matcher/LongitudMathcer.java
@@ -4,22 +4,36 @@ import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class LongitudMathcer extends FeatureMatcher {
     public LongitudMathcer(Matcher subMatcher, String featureDescription, String featureName) {
         super(subMatcher, featureDescription, featureName);
     }
 
+    /*
+    Defineix un Matcher personalitzat per a Hamcrest que proporcioni el Matcher de longitud per a un String.
+
+    Volem usar la classe ->FeatureMatcher.
+    Amb FeatureMatcher podem ajustar un Matcher existent, decidir quin camp de l'Objecte prova ha de coincidir i proporcionar un missatge d'error agradable. El constructor de FeatureMatcher té els següents arguments en aquest ordre:
+
+    El matcher que volem embolicar.
+    Una descripció de la funció que provem.
+    Una descripció del possible mismatch (desajustament).
+    L'únic mètode que hem de sobreescriure és featureValueOf (T actual), que retorna el valor que es passarà al mètode match () / matchesSafely (). Utilitza el seu comparador personalitzat en una prova per a comprovar si la cadena "Mordor" té una longitud de 8.
+
+    Ajusta la prova si és necessari.
+     */
+
+
     @Override
     public Object featureValueOf(Object o) {
         return null;
     }
-
     protected Integer featureValueOf(String texto) {
         return texto.length();
     }
-
-
-
 
 }
 
---
+++ src/test/java/S1_04_N3/AssertJTest.java
@@ -0,0 +1,156 @@
+package S1_04_N3;
+
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.OpenOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Optional;
+
+@DisplayName("AssertJ Test by Miquel")
+public class AssertJTest {
+
+    /*References:
+        https://www.baeldung.com/introduction-to-assertj
+        https://www.youtube.com/watch?v=oLEUHVP3kFU
+     */
+
+    // Exercise 1 --------------
+
+    @Test
+    @DisplayName("Ex1 - Object=Object by fields")
+    public void testMethodObjectByField(){
+        Person person1 = new Person("Miquel", 27);
+        Person person2 = new Person("Miquel", 27);
+        assertThat(person1).isEqualToComparingFieldByField(person2); //True
+        assertThat(person1).isEqualToComparingOnlyGivenFields(person2, "age");
+    }
+    @Test
+    @DisplayName("Ex1 - Object!=Object by fields")
+    public void testMethodNoObjectByField(){
+        Person person1 = new Person("Miquel", 27);
+        Person person2 = new Person("Miquel", 27);
+        assertThat(person1).isNotEqualTo(person2); //True
+    }
+
+    // Exercise 2 --------------
+
+    @Test
+    @DisplayName("Ex2 - Object=Object by memory location")
+    public void testMethodObjectMemoryLocation(){
+        Person person1 = new Person("Miquel", 27);
+        Person person2 = new Person("Miquel", 27);
+        assertThat(person1).isEqualTo(person2); //Error != memory location
+        assertThat(person1).isSameAs(person2);
+    }
+
+    // Exercise 3 --------------
+    //Una asserció que indiqui que dos arrays d'enters són idèntics.
+    @Test
+    @DisplayName("Ex3 - Array=Array")
+    public void testMethodArray(){
+        int[] array1 = {1,2,3,4,5};
+        int[] array2 = {1,2,3,4,5};
+        assertThat(array1).isEqualTo(array2);
+    }
+
+
+    //Exercise 4 ----------------
+    /*
+    Crea un arrayList contenidor de diversos tipus d'objectes (crea'ls també). Escriu una asserció per a verificar
+    l'ordre dels objectes en l'ArrayList segons han estat inserits.
+
+    Ara verifica que la llista anterior conté els objectes en qualsevol ordre.
+    Verifica que, en la llista anterior, un dels objectes s'ha afegit només una vegada. Deixa un dels elements sense afegir,
+    i verifica que la llista no conté aquest últim.
+     */
+
+    @Test
+    @DisplayName("Ex 4 - ArrayList Contains exactly Order")
+    public void testMethodArrayList1(){
+        ArrayList<Object> objArrayList = new ArrayList<>();
+        int numero = 1;
+        String string = "string";
+        Person person1 = new Person("miquel", 1);
+        objArrayList.add(numero);
+        objArrayList.add(string);
+        objArrayList.add(person1);
+        assertThat(objArrayList).containsExactly(numero, string, person1);
+        assertThat(objArrayList).containsSequence(numero, string, person1);
+    }
+
+    @Test
+    @DisplayName("Ex 4 - ArrayList Contains exactly by any Order")
+    public void testMethodArrayList2(){
+        ArrayList<Object> objArrayList = new ArrayList<>();
+        int numero = 1;
+        String string = "string";
+        Person person1 = new Person("miquel", 1);
+        objArrayList.add(numero);
+        objArrayList.add(string);
+        objArrayList.add(person1);
+        assertThat(objArrayList).containsExactlyInAnyOrder(string, numero,person1);
+    }
+
+    @Test
+    @DisplayName("Ex 4 - ArrayList Missing One element")
+    public void testMethodArrayList3(){
+        ArrayList<Object> objArrayList = new ArrayList<>();
+        int numero = 1;
+        String string = "string";
+        Person person1 = new Person("miquel", 1);
+        objArrayList.add(numero);
+        objArrayList.add(numero);
+        objArrayList.add(string);
+        //objArrayList.add(person1);
+
+//        assertThat(objArrayList).containsOnlyOnce(numero); //Error contains two units
+        assertThat(objArrayList).doesNotContainSequence(numero, numero, string, person1);
+    }
+
+    //Exercise 5 ----------------
+
+    @Test
+    @DisplayName("Ex 5 - HashMap asser one key")
+    public void testMethodHashMap(){
+        HashMap<Integer, String> integerStringHashMap = new HashMap<>();
+        integerStringHashMap.put(0, "zero");
+        integerStringHashMap.put(1, "one");
+        integerStringHashMap.put(2, "two");
+        assertThat(integerStringHashMap).containsKey(2);
+    }
+
+    //Exercise 6 ----------------
+
+
+    @Test
+    @DisplayName("Ex 6 - Assert Exception")
+    public void testMethodException(){
+        assertThatThrownBy(() -> {
+            Person person1 = new Person();
+            person1.throeExceptionMethod();
+        }).isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    @DisplayName("Ex 6 - Assert Exception 2")
+    public void testMethodException2(){
+        Person person1 = new Person();
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(person1::throeExceptionMethod);
+    }
+
+    //Exercise 7 ----------------
+
+    @Test
+    @DisplayName("Empty Optional")
+    public void testMethodEmptyOprtional(){
+        Optional<String> optionalS = Optional.empty();
+        assertThat(optionalS).isEmpty();
+    }
+
+
+}
